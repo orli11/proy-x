871cf11ae7aeaefb0b972ba8b6cb5ff2cd1d552b
ximena <xsmg06@outlook.com>
2023-11-21 17:50:44 -0600

    cambios para login.php

---
+++ Backend/Files/login.php
@@ -0,0 +1,29 @@
+<?php
+    include("../config/conexion.php");
+    $con = conectar();
+    $dataPost = file_get_contents('php://input');
+    $body = json_decode($dataPost, true);
+
+    if($body !== null) {
+        $email = $body['email'];
+        $pass = $body['pass'];
+
+        $queryUsuario = "SELECT * FROM usuarios WHERE usuario='$email'";
+        $validaUsuario = mysqli_query($con, $queryUsuario);
+
+        if($validaUsuario->num_rows > 0) {
+            $usuario = $validaUsuario->fetch_assoc();
+
+            if(password_verify($pass, $usuario['password'])) {
+                echo json_encode(['STATUS' => 'SUCCESS', 'MESSAGE' => 'Success', 'USUARIO' => $usuario]);
+            } else {
+                echo json_encode(['STATUS' => 'ERROR', 'MESSAGE' => 'Contraseñas no coinciden']);
+            }
+        } else {
+            echo json_encode(['STATUS' => 'ERROR', 'MESSAGE' => 'No se encontro usuario']);
+        }
+    } else {
+        http_response_code(400);
+        echo 'Invalid Data';
+    }
+?>  
