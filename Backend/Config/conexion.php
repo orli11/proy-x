906b5a06681d0105cef4afad1b71d7a58e63523f
AlejandroRG12 <garcia4312@hotmail.com>
2023-11-21 15:10:20 -0600

    Fix database connection and registration form

--- Backend/Config/conexion.php
+++ Backend/Config/conexion.php
@@ -1,11 +1,11 @@
 <?php
-    function conectar(){
+    function conectar() {
         $host = 'localhost:8889';
         $user = 'root';
-        $pass = "root";
-        $db = "x";
-        $conn = mysqli_connect($host, $user, $pass);
-        mysqli_select_db($conn, $db);
-        return $conn;
+        $pass = 'root';
+        $db = 'x';
+        $con = mysqli_connect($host, $user, $pass, $db);
+        return $con;
+        echo json_encode(['STATUS' => 'DB: CONEXION ESTABLECIDA']);
     }
 ?>
--- js/registro.js
+++ js/registro.js
@@ -1,14 +1,20 @@
 document.addEventListener('DOMContentLoaded', () =>{
+    //aletas
+    const url = window.location.search;
+    const params = new URLSearchParams(url);
+    const existe = params.get('existe');
+
+    //validaciones
     window.onload = function() {
-        var diaSelect = document.getElementById('dia');
-        var mesSelect = document.getElementById('mes');
-        var anoSelect = document.getElementById('ano');
+        let diaSelect = document.getElementById('dia');
+        let mesSelect = document.getElementById('mes');
+        let anoSelect = document.getElementById('ano');
 
         for (var i = 1; i <= 31; i++) {
             diaSelect.innerHTML += `<option value="${i}">${i}</option>`;
         }
 
-        var meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+        let meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
         for (var i = 0; i < meses.length; i++) {
             mesSelect.innerHTML += `<option value="${i + 1}">${meses[i]}</option>`;
         }
@@ -16,5 +22,16 @@ document.addEventListener('DOMContentLoaded', () =>{
         for (var i = 1940; i <= 2023; i++) {
             anoSelect.innerHTML += `<option value="${i}">${i}</option>`;
         }
+    }
+
+    //estado de alerta
+    if(existe) {
+        const alerta = document.getElementsByClassName('alert');
+        alerta[0].classList.remove('hide');
+        alerta[0].classList.add('show');
+        setTimeout(() => {
+            alerta[0].classList.remove('show');
+            alerta[0].classList.add('hide');
+        }, 3000);
     }
 })
