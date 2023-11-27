4f3c05938fab036a8f1e894b0ddab345b6d70f9b
AlejandroRG12 <garcia4312@hotmail.com>
2023-11-27 13:16:05 -0600

    ya añade comentarios al hilo

---
+++ Backend/Files/nuevoHilo.php
@@ -0,0 +1,21 @@
+<?php
+    include("../Config/conexion.php");
+    $conn = conectar();
+
+    $url_anterior = $_SERVER['HTTP_REFERER'];
+    $id_post = $_POST['id_post'];
+    $mensaje = $_POST['mensaje'];
+    //Establece la zona horaria de Ciudad de México
+    date_default_timezone_set('America/Mexico_City');
+    $fecha = date("Y-m-d H:i:s");
+
+    $queryInsert = "INSERT INTO hilo VALUES(null, '$id_post', '$mensaje', '$fecha')";
+    $result = mysqli_query($conn, $queryInsert);
+    
+    if($result){
+        echo json_encode(['STATUS' => 'SUCCESS', 'MESSAGE' => 'Post registrado']);
+        Header("Location: $url_anterior");
+    } else {
+        echo json_encode(['STATUS' => 'ERROR', 'MESSAGE' => 'Post no registrado']); 
+    }
+?>
