a90384d852abe93277519e530b2cb2fc18b86658
AlejandroRG12 <garcia4312@hotmail.com>
2023-11-28 01:31:34 -0600

    ya se hacen commentarios en los post asi que ya genera hilos

--- js/hilo.js
+++ js/hilo.js
@@ -1,7 +1,12 @@
+const postContainer = document.getElementById('postRespuestasHilo')
+const postCard = document.getElementById('cardPost').content
+const fragment = document.createDocumentFragment()
+
 var params = new URLSearchParams(window.location.search);
 var usuario = params.get('usuario');
 var fecha = params.get('fecha');
 var fromUser = params.get('fromUser');
+
 
 console.log(usuario);
 console.log(fecha); 
@@ -10,6 +15,7 @@ console.log(fromUser)
 document.addEventListener('DOMContentLoaded', () => {
     loadDataPost()
     TraerUserCard()
+    loadComments()
 })
 
 const loadDataPost = () => {
@@ -29,6 +35,7 @@ const loadDataPost = () => {
         console.log(posts)
         
         const idpost = document.getElementById('id_post');
+        const CommentFromUser = document.getElementById('CommentFromUser')
         // Encuentra los elementos HTML
         let titulo = document.querySelector('#titulo');
         let usuarioName = document.querySelector('.usuarioName');
@@ -37,12 +44,35 @@ const loadDataPost = () => {
         let fecha = document.querySelector('.fecha');
         
         // Asigna los valores del objeto JSON a los elementos HTML
+        console.log('ESTE ES EL NOMBRE QUE SE ASSIGNA AL POST: ', fromUser);
+        CommentFromUser.value = fromUser
         idpost.value = posts.id_pos;
         titulo.textContent = posts.titulo;
         usuarioName.textContent = posts.id_usu;
         comentario.textContent = posts.mensaje;
         fecha.textContent = posts.fecha;
     })
+}
+
+const loadComments = async () => {
+    fetch(`./Backend/Files/loadCommentsHilo.php?usuario=${usuario}&fecha=${fecha}`)
+    .then(async (response) => {
+        const items = await response.json()
+        console.log(items)
+        printComments(items.MESSAGE)
+    })
+}
+
+const printComments = post => {
+    postMessage.innerHTML = ''
+    post.forEach((item) => {
+        postCard.querySelector('.usuarioNameComment').textContent = item.usuario
+        postCard.querySelector('.fechaComment').textContent = item.fecha
+        postCard.querySelector('.Comment').textContent = item.mensaje
+        const clone = postCard.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    postContainer.appendChild(fragment)
 }
 
 const TraerUserCard = () => {
@@ -68,6 +98,9 @@ const TraerUserCard = () => {
         usuario.textContent = userCard.usuario;
         usuarioEmail.textContent = userCard.email;
 
+        //asignar el usuario desde el caul se esta viendo el post
+        id_usu = userCard.usuario;
+        //console.log('NOMBRE DEL USUARIO => ', id_usu);
     })
 }
 
